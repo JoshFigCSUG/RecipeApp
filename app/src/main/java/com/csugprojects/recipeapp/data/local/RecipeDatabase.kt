12a9c8d42c8db9d7a56173e4119357feb0c0ce62
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2025-10-03 15:17:45 -0600

    Update RecipeDatabase.kt
    update comments for organization and documentation

--- app/src/main/java/com/csugprojects/recipeapp/data/local/RecipeDatabase.kt
+++ app/src/main/java/com/csugprojects/recipeapp/data/local/RecipeDatabase.kt
@@ -4,8 +4,17 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [RecipeEntity::class], version = 2, exportSchema = false) // VERSION BUMPED TO 2
+/**
+ * RecipeDatabase is the abstract class that represents the local data source (Model Layer - M4 Design).
+ * It stores recipes marked as favorites, providing offline support (M4 Feature).
+ */
+@Database(entities = [RecipeEntity::class], version = 2, exportSchema = false)
+// The TypeConverters annotation ensures complex types like List<Ingredient> can be stored (M4 Design).
 @TypeConverters(Converters::class)
 abstract class RecipeDatabase : RoomDatabase() {
+
+    /**
+     * Exposes the Data Access Object (DAO) that allows the Repository to perform CRUD operations.
+     */
     abstract fun recipeDao(): RecipeDao
 }
