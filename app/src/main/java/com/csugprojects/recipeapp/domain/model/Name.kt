c00e6ebb3b288b12478f1c26ec57eb2ec0b9cd0a
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2025-10-03 15:40:02 -0600

    Update Name.kt
    update comments for organization and documentation

--- app/src/main/java/com/csugprojects/recipeapp/domain/model/Name.kt
+++ app/src/main/java/com/csugprojects/recipeapp/domain/model/Name.kt
@@ -1,6 +1,10 @@
 package com.csugprojects.recipeapp.domain.model
 
+/**
+ * Name is a versatile domain model used for listing filtered criteria (M6 Feature).
+ * It represents a single item like an ingredient or an area/cuisine.
+ */
 data class Name(
     val name: String,
-    val type: String // E.g., "Ingredient" or "Area" to help with UI logic
+    val type: String
 )
