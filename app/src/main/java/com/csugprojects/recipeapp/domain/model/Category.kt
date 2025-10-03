d7be470086c3da9e17e83c3bd61a802d6580624f
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2025-10-03 15:39:45 -0600

    Update Category.kt
    update comments for organization and documentation

--- app/src/main/java/com/csugprojects/recipeapp/domain/model/Category.kt
+++ app/src/main/java/com/csugprojects/recipeapp/domain/model/Category.kt
@@ -1,5 +1,9 @@
 package com.csugprojects.recipeapp.domain.model
 
+/**
+ * Category is a core domain model (Model Layer - M2/M4).
+ * It represents a single meal category, converted from a network DTO.
+ */
 data class Category(
     val id: String,
     val name: String,
