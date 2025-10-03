827d430731252bfad9ec3d5241a9ed25c90940b2
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2025-10-03 15:13:43 -0600

    Update Converters.kt
    update comments for organization and documentation

--- app/src/main/java/com/csugprojects/recipeapp/data/local/Converters.kt
+++ app/src/main/java/com/csugprojects/recipeapp/data/local/Converters.kt
@@ -5,12 +5,25 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.csugprojects.recipeapp.domain.model.Ingredient
 
+/**
+ * This class provides TypeConverters to the Room Database (M4 Data Persistence).
+ * Room cannot natively store complex objects like List<Ingredient>.
+ * These functions use the Gson library to convert the list to a storable JSON string and back again.
+ */
 class Converters {
+
+    /**
+     * Converts a list of Ingredient objects into a single JSON string for database storage.
+     */
     @TypeConverter
     fun fromIngredientList(list: List<Ingredient>): String {
         return Gson().toJson(list)
     }
 
+    /**
+     * Converts a JSON string retrieved from the database back into a List<Ingredient> object.
+     * This restores the original format for the Model layer logic.
+     */
     @TypeConverter
     fun toIngredientList(json: String): List<Ingredient> {
         val type = object : TypeToken<List<Ingredient>>() {}.type
